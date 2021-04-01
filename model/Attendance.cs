60e91e931533a89a340e67373ad9aeacc763b330
Aya Kamal <bd58296@gmail.com>
2021-04-01 15:50:46 +0200

    Initial commit

---
+++ model/Attendance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherAttendance.model
+{
+    class Attendance
+    {
+
+        public int TeacherId { get; set; }
+        public int CourseId { get; set; }
+        public int RoomId { get; set; }
+        public String StartTime { get; set; }
+        public String LeaveTime { get; set; }
+        public String Comment { get; set; }
+
+        public Attendance(int teacherId, int courseId, int RoomId, string startTime, string leaveTime, string comment ) {
+            this.TeacherId = teacherId;
+            this.CourseId = courseId;
+            this.RoomId = RoomId;
+            this.StartTime = startTime;
+            this.LeaveTime = leaveTime;
+            this.Comment = comment;
+        }
+
+    }
+}
